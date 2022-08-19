9f03a030a09a75de73e57cc38264868eca45dc00
holgermueller <9jholger@gmail.com>
2022-08-18 20:28:08 -0500

    another attempt at deploying

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ npm run build
 cd dist
 
 git init
+git checkout -b main
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:Holgermueller/thevoid.git master:gh-pages
+git push -f git@github.com:Holgermueller/thevoid.git main:gh-pages
 
 cd -
